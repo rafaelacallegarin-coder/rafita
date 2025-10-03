781347294de0f7d264a339d492d8eda6df860a03
rafaelacallegarin-coder <rafaela.callegarin@escola.pr.gov.br>
2025-10-03 08:05:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+function tocarSom(id) {
+	const audios = document.querySelectorAll('audio');
+	audios.forEach(audio => {
+		audio.pause();
+		audio.currentTime = 0;
+	});
+	const audioSelecionado = document.querySelector(id);
+	if (audioSelecionado) {
+		audioSelecionado.play();
+	}
+}
